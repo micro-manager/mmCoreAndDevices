micro-manager/mmCoreAndDevices

commit f2eaf24b158740dd2b360d47126a14bdcbe06a64

Made shutterState and switchState local, so that mulitple Arduinos can be used simutanuously
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@13735 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/Arduino/Arduino.cpp
@@ -32,8 +32,6 @@ const char* g_DeviceNameArduinoInput = "Arduino-Input";
 
 
 // Global info about the state of the Arduino.  This should be folded into a class
-unsigned g_switchState = 0;
-unsigned g_shutterState = 0;
 const int g_Min_MMVersion = 1;
 const int g_Max_MMVersion = 2;
 const char* g_versionProp = "Version";
@@ -102,7 +100,9 @@ MODULE_API void DeleteDevice(MM::Device* pDevice)
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~
 //
 CArduinoHub::CArduinoHub() :
-initialized_ (false)
+   initialized_ (false),
+   switchState_ (0),
+   shutterState_ (0)
 {
    portAvailable_ = false;
    invertedLogic_ = false;
@@ -631,8 +631,8 @@ int CArduinoSwitch::OnState(MM::PropertyBase* pProp, MM::ActionType eAct)
    {
       long pos;
       pProp->Get(pos);
-      g_switchState = pos;
-      if (g_shutterState > 0)
+      hub->SetSwitchState(pos);
+      if (hub->GetSwitchState() > 0)
          return WriteToPort(pos);
    }
    else if (eAct == MM::IsSequenceable)                                      
@@ -1369,10 +1369,11 @@ int CArduinoShutter::WriteToPort(long value)
 
 int CArduinoShutter::OnOnOff(MM::PropertyBase* pProp, MM::ActionType eAct)
 {
+   CArduinoHub* hub = static_cast<CArduinoHub*>(GetParentHub());
    if (eAct == MM::BeforeGet)
    {
       // use cached state
-      pProp->Set((long)g_shutterState);
+      pProp->Set((long)hub->GetShutterState());
    }
    else if (eAct == MM::AfterSet)
    {
@@ -1382,10 +1383,10 @@ int CArduinoShutter::OnOnOff(MM::PropertyBase* pProp, MM::ActionType eAct)
       if (pos == 0)
          ret = WriteToPort(0); // turn everything off
       else
-         ret = WriteToPort(g_switchState); // restore old setting
+         ret = WriteToPort(hub->GetSwitchState()); // restore old setting
       if (ret != DEVICE_OK)
          return ret;
-      g_shutterState = pos;
+      hub->SetShutterState(pos);
       changedTime_ = GetCurrentMMTime();
    }
 

DeviceAdapters/Arduino/Arduino.h
@@ -68,6 +68,10 @@ public:
       return ReadFromComPort(port_.c_str(), answer, maxLen, bytesRead);
    }
    static MMThreadLock& GetLock() {return lock_;}
+   void SetShutterState(unsigned state) {shutterState_ = state;}
+   void SetSwitchState(unsigned state) {switchState_ = state;}
+   unsigned GetShutterState() {return shutterState_;}
+   unsigned GetSwitchState() {return switchState_;}
 
 private:
    int GetControllerVersion(int&);
@@ -78,6 +82,8 @@ private:
    bool timedOutputActive_;
    int version_;
    static MMThreadLock lock_;
+   unsigned switchState_;
+   unsigned shutterState_;
 };
 
 class CArduinoShutter : public CShutterBase<CArduinoShutter>  

commit 15c2500148d26950e1c8a5db025da3bfc74ef804

Arduino device adapter, fix bug introduced in r13735
git-svn-id: https://valelab.ucsf.edu/svn/micromanager2/trunk@14441 d0ab736e-dc22-4aeb-8dc9-08def0aa14fd

DeviceAdapters/Arduino/Arduino.cpp
@@ -215,7 +215,7 @@ MM::DeviceDetectionStatus CArduinoHub::DetectDevice(void)
          GetCoreCallback()->SetDeviceProperty(port_.c_str(), "DelayBetweenCharsMs", "0");
          MM::Device* pS = GetCoreCallback()->GetDevice(this, port_.c_str());
          pS->Initialize();
-         // The first second or so after opening the serial port, the Arduino is waiting for firmwareupgrades.  Simply sleep 1 second.
+         // The first second or so after opening the serial port, the Arduino is waiting for firmwareupgrades.  Simply sleep 2 seconds.
          CDeviceUtils::SleepMs(2000);
          MMThreadGuard myLock(lock_);
          PurgeComPort(port_.c_str());
@@ -632,7 +632,7 @@ int CArduinoSwitch::OnState(MM::PropertyBase* pProp, MM::ActionType eAct)
       long pos;
       pProp->Get(pos);
       hub->SetSwitchState(pos);
-      if (hub->GetSwitchState() > 0)
+      if (hub->GetShutterState() > 0)
          return WriteToPort(pos);
    }
    else if (eAct == MM::IsSequenceable)                                      