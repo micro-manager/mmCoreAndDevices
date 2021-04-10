989f25050258990f65d8befac440ba55ee16a05d
nicholas.anthony@northwestern.edu <nicholas.anthony@northwestern.edu>
2021-04-10 15:30:02 -0500

    add script to activate dev environment

---
+++ buildscripts/activateVS.bat
@@ -0,0 +1,3 @@
+for /f "usebackq delims=#" %%a in (`"%programfiles(x86)%\Microsoft Visual Studio\Installer\vswhere" -latest -property installationPath`) do set VsDevCmd_Path=%%a\Common7\Tools\VsDevCmd.bat
+echo %VsDevCmd_Path%
+call "%VsDevCmd_Path%" -arch=amd64
