3b8f2e5c5601d495bec3e45cb98aec04f1e25526
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2022-07-27 17:01:50 -0700

    move resource.h to project files

---
+++ DeviceAdapters/MeadowlarkLC/resource.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MeadowlarkLC.rc
+//
+#define TEXTFILE                        256
+#define IDR_TEXT1                       701
+#define IDR_TEXT2                       702
+#define IDR_B14181                      703
+#define IDR_B14182                      704
+#define IDR_B14183                      705
+#define IDR_T001                        706
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        141
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           104
+#endif
+#endif
