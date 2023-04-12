108d5d910ee9ed8ad2149340a40a8c17b87ca482
Ivo Vellekoop <i.m.vellekoop@utwente.nl>
2023-04-12 18:27:40 +0200

    Drafted Python property system

--- DeviceAdapters/PyDevice/test.py
+++ DeviceAdapters/PyDevice/test.py
@@ -1,4 +1,19 @@
+def parse_options(object, values):
+    for o in values # object.options.items():
+        setattr(obj, o[0], o[1])
+
 class Device:
-    options = {}
+    options = {
+        "width" : {
+            "default" : 10,
+            "min" : 0,
+            "max" : 100
+        }
+    }
+    def __init__(**kwargs):
+        parse_options(self, kwargs)
+        
     
 
+
+d = Device(width = 10)
