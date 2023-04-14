b3585427397e13797e56d64ca8fea6240b27f52e
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2023-04-14 15:16:55 -0700

    remove old win32 code

--- DeviceAdapters/MeadowlarkLC/MeadowlarkLC.cpp
+++ DeviceAdapters/MeadowlarkLC/MeadowlarkLC.cpp
@@ -23,10 +23,6 @@
 //                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
 
 
-#ifdef WIN32
-//   #include <windows.h>
-#define snprintf _snprintf
-#endif
 
 #include <cstdio>
 #include <string>
