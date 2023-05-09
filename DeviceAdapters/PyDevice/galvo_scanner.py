76adb45032d003951fc77f52b0962107d1c049ad
Jeroen Doornbos <j.h.doornbos@student.utwente.nl>
2023-05-09 10:24:41 +0200

    Added exposure & dwelltime

--- DeviceAdapters/PyDevice/galvo_scanner.py
+++ DeviceAdapters/PyDevice/galvo_scanner.py
@@ -17,11 +17,11 @@ class GalvoScanner:
         parse_options(self, kwargs)
         self.resized = True
 
-    def galvo_scan(self,buffer,invert,resolution,input,output,scanpadding,delay,bidirectional,zoom,input_range):
+    def galvo_scan(self,buffer,invert,resolution,input,output,scanpadding,delay,bidirectional,zoom,input_range,exposure):
 
         im = single_capture(invert_values=invert,resolution=resolution,input_mapping=input,output_mapping=output,
                             scanpaddingfactor=scanpadding,delay=delay,bidirectional=bidirectional,zoom=zoom,
-                            input_range=input_range)
+                            input_range=input_range,duration=exposure,)
         buffer[:, :] = np.reshape(im, resolution)
 
 
@@ -46,10 +46,19 @@ class Camera:
     def wait(self):
         self.galvo_scan.galvo_scan(self.image,[self._invert],[self._width, self._height],[self._input_mapping],
                                    [self._xmirror_mapping,self._ymirror_mapping],[self._scan_padding],[self._delay],
-                                   [self._bidirectional],[self._zoom],[self._input_min, self._input_max])
+                                   [self._bidirectional],[self._zoom],[self._input_min, self._input_max],[self._exposure_ms/1000])
 
     def on_resized(self, value):
         self.resized = True
+#        self._exposure_ms = (self._dwelltime_us / 1000) * (self._width * self._height)
+        return value
+
+    def on_dwelltime(self,value):
+        self._exposure_ms = (value/1000)*(self._width*self._height)
+        return value
+
+    def on_exposure(self,value):
+        self._dwelltime_us = (value*1000) / (self._width * self._height)
         return value
 
 
@@ -64,8 +73,11 @@ class Camera:
     input_min = float_property(min=-1.5, max=1.5, default=-1)
     input_max = float_property(min=-1.5, max=1.5, default=1)
 
+
+    dwelltime_us = float_property(min=0.5, default=4, on_update=on_dwelltime)
+
     delay = int_property(min=0, max=4096, default=0)
-    exposure_ms = float_property(min=0.0, default=100)
+    exposure_ms = float_property(min=0.0, default=600, on_update=on_exposure)
     zoom = float_property(min=1, max=1000, default=1)
     scan_padding = float_property(min=1, max=4, default=1)
     galvo_scan = object_property(default=GalvoScanner())
