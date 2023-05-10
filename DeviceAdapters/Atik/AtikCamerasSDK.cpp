be659c706dcccdd508258342297f68b6b35dd591
Mark A. Tsuchida <matsuchida@wisc.edu>
2023-05-10 11:38:36 -0500

    Atik: Fix include path
    And avoid directly including .cpp file from SDK in the project.

---
+++ DeviceAdapters/Atik/AtikCamerasSDK.cpp
@@ -0,0 +1,8 @@
+// This file simply includes the required code from the Atik SDK (the code to
+// load and call the DLLs is distributed as AtikCameras.cpp).
+//
+// This intermediary file is used so that the path to the SDK is determined by
+// project properties, rather than being hard-coded in the project. That way,
+// the path is set in one place only.
+
+#include "AtikCameras.cpp"
