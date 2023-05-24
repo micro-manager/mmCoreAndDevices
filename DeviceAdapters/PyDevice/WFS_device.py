5d970c01f2db59b40a1cf54ac188f50df41a793f
Jeroen Doornbos <j.h.doornbos@student.utwente.nl>
2023-05-24 10:26:27 +0200

    Fixed binning, improved wfs

--- DeviceAdapters/PyDevice/WFS_device.py
+++ DeviceAdapters/PyDevice/WFS_device.py
@@ -10,7 +10,7 @@ from Fourier import FourierDualRef
 from SLMwrapper import SLM, set_circular_geometry, test
 from test_cam import float_property, int_property, string_property, object_property, base_property, bool_property, parse_options
 #from WFS_functions import manual_slm_setup, select_roi, take_image, single_capt, point_capt, make_point_mean, select_point, example_mean, full_experiment_ssa
-from WFS_functions import manual_slm_setup,select_point,full_experiment_fourier, full_experiment_ssa, wfs_procedure
+from WFS_functions import manual_slm_setup,select_point,full_experiment_fourier, full_experiment_ssa, wfs_procedure,slm_setup
 from galvo_scanner import Camera
 class SLM_device:
 
@@ -24,6 +24,7 @@ class SLM_device:
     def delete_slm(self):
         self.s.destroy()
 
+    wavelength_nm = float_property(min=400, default=804, max=1600)
     slm_number = int_property(min = 0,default=0)
 
 
@@ -83,13 +84,15 @@ class WFS:
     def take_image(self):
         self.camera_object.trigger()
         self.camera_object.wait()
-        return self.camera_object.image
+
+        return np.reshape(self.camera_object.image, [self.camera_object._height,self.camera_object._width],order='C')
 
     def on_execute(self,value):
 
 
 
         if value:
+
             if self.slm_object == None:
                 self._ValueError(f"A SLM object needs to be connected in order to wavefront shape")
 
@@ -99,13 +102,58 @@ class WFS:
             if self.algorithm == None:
                 self._ValueError(f"An algorithm object needs to be added in order to wavefront shape")
 
-            wfs_procedure(self.algorithm, self.slm_object.make_slm(),self.take_image)
+            self.optimised_wf, _ = wfs_procedure(self.algorithm, self.slm_object,self.take_image)
 
 
         self._execute = False
         return value
 
+    def on_optimised_wf(self,value):
+        if not value:
+            if self.active_slm:
+                self.slm.destroy()
+                self.active_slm = False
+        else:
+            if not self.active_slm:
+                self.slm = self.slm_object.make_slm()
+                slm_setup(self.slm)
+
+            if isinstance(self.optimised_wf,np.ndarray):
+                self.slm.set_data(self.optimised_wf)
+                self.slm.update(10)
+                self.active_slm = True
+
+            else:
+                self._ValueError(f"No optimised wavefront was found in memory")
+
+        return value
+
+    def on_flat_wf(self,value):
+        if not value:
+            if self.active_slm:
+                self.slm.destroy()
+                self.active_slm = False
+        else:
+            if not self.active_slm:
+                self.slm = self.slm_object.make_slm()
+                slm_setup(self.slm)
+
+            self.slm.set_data(0)
+            self.slm.update(10)
+            self.active_slm = True
+
+        return value
+
+
+
+    active_slm = False
+    slm = None
+
+
     algorithm = object_property()
+    optimised_wf = None
     slm_object = object_property()
     camera_object = object_property()
     execute = bool_property(default=0, on_update=on_execute)
+    show_optimised_wavefront = bool_property(default=0, on_update=on_optimised_wf)
+    show_flat_wavefront = bool_property(default=0, on_update=on_flat_wf)
