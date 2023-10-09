3f8a9ec6ef249156a59e34ca24e241098742a7a0
Matthew Anzelmi <manzelmi@Thorlabs.com>
2023-10-09 14:12:23 -0400

    Bringing over initialization code

--- DeviceAdapters/ThorlabsCHROLIS/ThorlabsChrolisDeviceWrapper.cpp
+++ DeviceAdapters/ThorlabsCHROLIS/ThorlabsChrolisDeviceWrapper.cpp
@@ -12,7 +12,35 @@ ThorlabsChrolisDeviceWrapper::~ThorlabsChrolisDeviceWrapper()
 
 int ThorlabsChrolisDeviceWrapper::InitializeDevice(std::string serialNumber = "")
 {
+    int err = 0;
+    ViUInt32 numDevices;
+    CDeviceUtils::SleepMs(2000);
+    err = TL6WL_findRsrc(NULL, &numDevices);
+    if (err != 0)
+    {
+        LogMessage("Find Resource Failed: " + std::to_string(err));
+        return DEVICE_ERR;
+    }
+    if (numDevices == 0)
+    {
+        LogMessage("Chrolis devices not found"); // to log file 
+        return DEVICE_ERR;
+    }
 
+    ViChar resource[512] = "";
+    err = TL6WL_getRsrcName(NULL, 0, resource);
+    if (err != 0)
+    {
+        LogMessage("Get Resource Failed: " + std::to_string(err));
+        return DEVICE_ERR;
+    }
+
+    err = TL6WL_init(resource, false, false, &deviceHandle_);
+    if (err != 0)
+    {
+        LogMessage("Initialize Failed: " + std::to_string(err));
+        return DEVICE_ERR;
+    }
 	return 0;
 }
 
