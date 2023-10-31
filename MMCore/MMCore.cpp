5c5053666cfd20f821568206c696a3a6d87d89a1
Mark A. Tsuchida <matsuchida@wisc.edu>
2023-10-31 18:08:31 -0500

    MMCore: Increment major version to 11
    For removal of deprecated CMMCore functions and config file commands.

--- MMCore/MMCore.cpp
+++ MMCore/MMCore.cpp
@@ -101,7 +101,7 @@ using namespace std;
  * (Keep the 3 numbers on one line to make it easier to look at diffs when
  * merging/rebasing.)
  */
-const int MMCore_versionMajor = 10, MMCore_versionMinor = 7, MMCore_versionPatch = 0;
+const int MMCore_versionMajor = 11, MMCore_versionMinor = 0, MMCore_versionPatch = 0;
 
 
 ///////////////////////////////////////////////////////////////////////////////
