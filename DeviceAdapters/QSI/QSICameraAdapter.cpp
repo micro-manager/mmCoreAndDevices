a58eb730808c9c8f6261e7174bee8a8706fff4be
Atik-Callum <129527371+Atik-Callum@users.noreply.github.com>
2024-01-02 13:58:46 +0000

    Added license

--- DeviceAdapters/QSI/QSICameraAdapter.cpp
+++ DeviceAdapters/QSI/QSICameraAdapter.cpp
@@ -4,21 +4,9 @@
 // SUBSYSTEM:     DeviceAdapters
 //-----------------------------------------------------------------------------
 // DESCRIPTION:   QSI camera adapter for use with Micro-Manager
-//                
-// AUTHOR:        Mike Barry, http://www.qsimaging.com
-//                
-// COPYRIGHT:     Quantum Scientific Imaging, Inc. 2013
+//                                
+// COPYRIGHT:     Quantum Scientific Imaging, Inc. 2024
 //
-// LICENSE:       This file is distributed under the BSD license.
-//                License text is included with the source distribution.
-//
-//                This file is distributed in the hope that it will be useful,
-//                but WITHOUT ANY WARRANTY; without even the implied warranty
-//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//
-//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
 
 #include "QSICameraAdapter.h"
 #include "QSIToolkit.h"
--- DeviceAdapters/QSI/QSICameraAdapter.h
+++ DeviceAdapters/QSI/QSICameraAdapter.h
@@ -4,21 +4,8 @@
 // SUBSYSTEM:     DeviceAdapters
 //-----------------------------------------------------------------------------
 // DESCRIPTION:   QSI camera adapter for use with Micro-Manager
-//                
-// AUTHOR:        Mike Barry, http://www.qsimaging.com
-//                
-// COPYRIGHT:     Quantum Scientific Imaging, Inc. 2013
-//
-// LICENSE:       This file is distributed under the BSD license.
-//                License text is included with the source distribution.
-//
-//                This file is distributed in the hope that it will be useful,
-//                but WITHOUT ANY WARRANTY; without even the implied warranty
-//                of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//
-//                IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-//                CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-//                INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES.
+//            
+// COPYRIGHT:     Quantum Scientific Imaging, Inc. 2024
 //
 
 #ifndef _QSICAMERAADAPTER_H_
