f345c8ff4b8dd9237638a26840886aacbecacd09
Henry Pinkard <7969470+henrypinkard@users.noreply.github.com>
2025-03-07 17:48:29 -0800

    allow snap to call insertImage instead of keeping its own buffer

--- MMCore/Devices/CameraInstance.h
+++ MMCore/Devices/CameraInstance.h
@@ -20,6 +20,10 @@
 #pragma once
 
 #include "DeviceInstanceBase.h"
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include "../FrameBuffer.h"
 
 
 class CameraInstance : public DeviceInstanceBase<MM::Camera>
@@ -82,4 +86,23 @@ public:
    int ClearExposureSequence();
    int AddToExposureSequence(double exposureTime_ms);
    int SendExposureSequence() const;
+
+   /**
+    * Checks if the camera is currently snapping an image.
+    * Thread-safe method that can be called from any thread.
+    * @return true if the camera is currently snapping an image, false otherwise
+    */
+   bool IsSnapping() const;
+   void StoreSnappedImage(const unsigned char* buf, unsigned width, unsigned height, 
+                          unsigned byteDepth, unsigned nComponents);
+
+private:
+   // Atomic flag to track if the camera is currently snapping an image
+   std::atomic<bool> isSnapping_{false};
+   
+   // Frame buffer to store captured images
+   mm::FrameBuffer snappedImage_;
+
+   std::mutex imageMutex_;
+   std::condition_variable imageAvailable_;
 };
