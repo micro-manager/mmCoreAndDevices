b8de737b2f2d92f64e03fbd79553ad09dd2b9c68
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-03-18 16:39:40 -0500

    Bump device interface _and_ MMCore version

--- MMDevice/MMDevice.h
+++ MMDevice/MMDevice.h
@@ -28,7 +28,7 @@
 // Header version
 // If any of the class definitions changes, the interface version
 // must be incremented
-#define DEVICE_INTERFACE_VERSION 71
+#define DEVICE_INTERFACE_VERSION 72
 ///////////////////////////////////////////////////////////////////////////////
 
 // N.B.
