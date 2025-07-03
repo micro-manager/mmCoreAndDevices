2ea8f4a21561ce24948901563b1c850dcdd72c28
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-07-03 14:30:40 -0500

    Bump MMCore minor version (to 11.9.0)

--- MMCore/MMCore.cpp
+++ MMCore/MMCore.cpp
@@ -112,7 +112,7 @@
  * (Keep the 3 numbers on one line to make it easier to look at diffs when
  * merging/rebasing.)
  */
-const int MMCore_versionMajor = 11, MMCore_versionMinor = 8, MMCore_versionPatch = 0;
+const int MMCore_versionMajor = 11, MMCore_versionMinor = 9, MMCore_versionPatch = 0;
 
 
 ///////////////////////////////////////////////////////////////////////////////
