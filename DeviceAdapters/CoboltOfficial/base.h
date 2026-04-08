d01c510dd46666b859913f6f1e8ab102ab6e9750
Lukas Kalinski <lukas.kalinski@coboltlasers.com>
2026-04-08 14:16:58 +0200

    Version bump

--- DeviceAdapters/CoboltOfficial/base.h
+++ DeviceAdapters/CoboltOfficial/base.h
@@ -60,7 +60,7 @@ namespace return_code
     const int unsupported_device_property_value = 101006;
 }
 
-#define COBOLT_MM_DRIVER_VERSION "1.0.6"
+#define COBOLT_MM_DRIVER_VERSION "1.0.7"
 
 NAMESPACE_COBOLT_END
 
